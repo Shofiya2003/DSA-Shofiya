29a95a2520634603ba608bcce7f1b65f3e9d5656
Shofiya2003 <shofiyabootwala@gmail.com>
2023-01-18 22:52:08 +0530

    added unioun by size method to distjointset class

--- Graphs/DisjointSet.java
+++ Graphs/DisjointSet.java
@@ -4,11 +4,13 @@ import java.util.List;
 public class DisjointSet {
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent=new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
     public DisjointSet(int n){
         for(int i=0;i<=n;i++){
             this.rank.add(0);
             this.parent.add(i);
+            size.add(1);
         }
     }
 
@@ -35,5 +37,19 @@ public class DisjointSet {
             rank.set(pu,rank.get(pu)+1);
         }
 
+    }
+
+    public void unionBySize(int u,int v){
+        int pu = getUltimateParent(u);
+        int pv = getUltimateParent(v);
+
+        if(size.get(pu)>size.get(pv)){
+            parent.set(pv,pu);
+            size.set(pu,size.get(pv)+size.get(pu));
+        }
+        else{
+            parent.set(pu,pv);
+            size.set(pv,size.get(pv)+size.get(pu));
+        }
     }
 }
